8746af1ec34700c2e7e83db0f40602b3b5009702
Yen Tran <tran.yentran@gmail.com>
2016-12-15 23:24:26 +0000

    adds basic routes

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    mongoose = require("mongoose");
+    
+mongoose.connect('mongodb://localhost/auth_demo_app');
     
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
     res.render('home');
+});
+
+app.get('/secret', function(req, res){
+    res.render('secret');
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
